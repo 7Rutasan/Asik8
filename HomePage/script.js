eb43ce72dd37f6b2608ebc29c1f31508b9a18a20
7rutasan <7rutasan@gmail.com>
2025-02-04 15:26:49 +0500

    Update2

--- HomePage/script.js
+++ HomePage/script.js
@@ -3,8 +3,11 @@ console.log("Hello, welcome to my website!");
 console.log("Take your performance to the next level with our expertly designed Programs for Athletes. Created for those who strive for excellence, our programs focus on building strength, enhancing endurance, and maximizing flexibility while ensuring proper recovery and injury prevention. ");
 console.log("Price: $" + (350+150)*1.3);
 
+let heroTextPart1 = "Restore your health with cutting-edge digital technologies — Kazakhstan's first digital rehabilitation center";
+let heroTextPart2 = "offers innovative solutions for your complete recovery.";
+
 // here we store text for hero section
-let heroText = "Restore your health with cutting-edge digital technologies — Kazakhstan's first digital rehabilitation center offers innovative solutions for your complete recovery.";
+let heroText = `${heroTextPart1} ${heroTextPart2}`;
 /*
 and then 
 put it into paragraph
@@ -30,7 +33,7 @@ contactForm.addEventListener("submit", function(event) {
 
 
     if (userName) {
-        formHeader.innerText = `Thank you, ${userName}! We will get back to you soon.`; 
+        formHeader.innerText = `Thank you, ${userName}! We will get back to you soon.`.toUpperCase();; 
     }
     
     contactForm.reset();
--- Services/script.js
+++ Services/script.js
@@ -6,14 +6,49 @@ let priceService5 = 900;
 
 let tax = 1.3;
 
-document.getElementById("price1").innerText = "Price: $" + priceService1 * tax;
-document.getElementById("price2").innerText = "Price: $" + priceService2 * tax;
-document.getElementById("price3").innerText = "Price: $" + priceService3 * tax;
-document.getElementById("price4").innerText = "Price: $" + priceService4 * tax;
-document.getElementById("price5").innerText = "Price: $" + priceService5 * tax;
+document.getElementById("price1").innerText = "Price: $" + (priceService1 * tax).toFixed();
+document.getElementById("price2").innerText = "Price: $" + (priceService2 * tax).toFixed();
+document.getElementById("price3").innerText = "Price: $" + (priceService3 * tax).toFixed();
+document.getElementById("price4").innerText = "Price: $" + (priceService4 * tax).toFixed();
+document.getElementById("price5").innerText = "Price: $" + (priceService5 * tax).toFixed();
 
 let packageOfThree = priceService1 + priceService3 + priceService4;
 console.log("Post-operative Rehabilitation + Sports Injury Recovery + Preventive Health and Wellness: $" + packageOfThree)
 
 priceService5 = 1000;
 document.getElementById("price5").innerText = "Price: $" + priceService5 * tax;
+
+
+let allServices = "Post-operative Rehabilitation Chronic Illness Management Programs Sports Injury Recovery (Tailored to Sports) Preventive Health and Wellness Programs for Athletes";
+
+console.log("Starts from index", allServices.indexOf("Chronic Illness Management Programs"));
+
+
+let totalPatients = 987654321234567890123456789n; 
+let revenuePerPatient = 1500000n;
+
+let totalRevenue = totalPatients * revenuePerPatient; 
+
+console.log(`Total patients: ${totalPatients}`);
+console.log(`Revenue per patient: $${revenuePerPatient}`);
+console.log(`Total Revenue: $${totalRevenue}`); 
+
+console.log(1*Number.MAX_VALUE);
+
+
+
+let programs = [
+    "Post-operative Rehabilitation",
+    "Chronic Illness Management Programs",
+    "Sports Injury Recovery (Tailored to Sports)",
+    "Preventive Health and Wellness",
+    "Programs for Athletes"
+];
+
+document.getElementById("service1").innerText = programs[0];
+document.getElementById("service2").innerText = programs[1];
+document.getElementById("service3").innerText = programs[2];
+document.getElementById("service4").innerText = programs[3];
+document.getElementById("service5").innerText = programs[4];
+
+
